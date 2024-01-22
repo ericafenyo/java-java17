c1ebfea106bf8f3e8674be6a6cb5e81f49f7daad
ericafenyo <ericafenyo@yahoo.com>
2024-01-22 11:20:24 +0100

    Add default method to sum ages

--- src/test/java/java17/ex01/Method_01_Test.java
+++ src/test/java/java17/ex01/Method_01_Test.java
@@ -17,8 +17,15 @@ public class Method_01_Test {
     interface IDao {
         List<Person> findAll();
 
+
         // TODO créer une méthode int sumAge()
         // TODO Cette méthode retourne le résultat de l'addition des ages des personnes
+        default int sumAge() {
+            return findAll().stream()
+                    .map(Person::getAge)
+                    .mapToInt(Integer::intValue)
+                    .sum();
+        }
     }
     // end::IDao[]
 
@@ -48,7 +55,7 @@ public class Method_01_Test {
         DaoA daoA = new DaoA();
 
         // TODO invoquer la méthode sumAge pour que le test soit passant
-        int result = 0;
+        int result = daoA.sumAge();
 
         assert result == 210;
     }
@@ -59,7 +66,7 @@ public class Method_01_Test {
         DaoB daoB = new DaoB();
 
         // TODO invoquer la méthode sumAge pour que le test soit passant
-        int result = 0;
+        int result = daoB.sumAge();
 
         assert result == 5050;
 
